7c16f08745423e2d473dcf8e04218eaebe0956df
Lesley <lesley.deveber@gmail.com>
2024-08-24 07:54:28 -0400

    update tab names

--- src/components/layout/__tests__/Body.test.tsx
+++ src/components/layout/__tests__/Body.test.tsx
@@ -5,50 +5,50 @@ import { describe, expect, test } from "vitest";
 import Body from "../Body";
 
 describe("Tab List Header", () => {
-  test("should render Windows tab", async () => {
+  test("should render Tab tab", async () => {
     const { getByRole } = render(<Body />);
 
     await waitFor(() => {
       expect(getByRole("tablist")).toBeVisible();
     });
 
-    expect(getByRole("tab", { name: "Windows" })).toBeVisible();
-    expect(getByRole("tab", { name: "Charts" })).toBeVisible();
-    expect(getByRole("tab", { name: "Snoozed" })).toBeVisible();
+    expect(getByRole("tab", { name: "Tab" })).toBeVisible();
+    expect(getByRole("tab", { name: "Count" })).toBeVisible();
+    expect(getByRole("tab", { name: "Snooze" })).toBeVisible();
 
-    expect(getByRole("tab", { selected: true })).toHaveTextContent("Windows");
-    expect(getByRole("tabpanel", { name: "Windows" })).toBeVisible();
+    expect(getByRole("tab", { selected: true })).toHaveTextContent("Tab");
+    expect(getByRole("tabpanel", { name: "Tab" })).toBeVisible();
   });
 
-  test("should render Charts tab", async () => {
+  test("should render Count tab", async () => {
     const user = userEvent.setup();
     const { getByRole } = render(<Body />);
 
     expect(getByRole("tablist")).toBeVisible();
 
-    await user.click(getByRole("tab", { name: "Charts" }));
+    await user.click(getByRole("tab", { name: "Count" }));
 
-    expect(getByRole("tab", { name: "Windows" })).toBeVisible();
-    expect(getByRole("tab", { name: "Charts" })).toBeVisible();
-    expect(getByRole("tab", { name: "Snoozed" })).toBeVisible();
+    expect(getByRole("tab", { name: "Tab" })).toBeVisible();
+    expect(getByRole("tab", { name: "Count" })).toBeVisible();
+    expect(getByRole("tab", { name: "Snooze" })).toBeVisible();
 
-    expect(getByRole("tab", { selected: true })).toHaveTextContent("Charts");
-    expect(getByRole("tabpanel", { name: "Charts" })).toBeVisible();
+    expect(getByRole("tab", { selected: true })).toHaveTextContent("Count");
+    expect(getByRole("tabpanel", { name: "Count" })).toBeVisible();
   });
 
-  test("should render Snoozed tab", async () => {
+  test("should render Snooze tab", async () => {
     const user = userEvent.setup();
     const { getByRole } = render(<Body />);
 
     expect(getByRole("tablist")).toBeVisible();
 
-    await user.click(getByRole("tab", { name: "Snoozed" }));
+    await user.click(getByRole("tab", { name: "Snooze" }));
 
-    expect(getByRole("tab", { name: "Windows" })).toBeVisible();
-    expect(getByRole("tab", { name: "Charts" })).toBeVisible();
-    expect(getByRole("tab", { name: "Snoozed" })).toBeVisible();
+    expect(getByRole("tab", { name: "Tab" })).toBeVisible();
+    expect(getByRole("tab", { name: "Count" })).toBeVisible();
+    expect(getByRole("tab", { name: "Snooze" })).toBeVisible();
 
-    expect(getByRole("tab", { selected: true })).toHaveTextContent("Snoozed");
-    expect(getByRole("tabpanel", { name: "Snoozed" })).toBeVisible();
+    expect(getByRole("tab", { selected: true })).toHaveTextContent("Snooze");
+    expect(getByRole("tabpanel", { name: "Snooze" })).toBeVisible();
   });
 });
