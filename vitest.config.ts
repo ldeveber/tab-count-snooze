6065d5f9535da9e70ba4cdeca80bd03ebc85125c
Lesley <lesley.deveber@gmail.com>
2025-04-19 22:21:21 -0400

    [maint] enable test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,28 @@ export default defineConfig({
     mockReset: true,
     restoreMocks: true,
     setupFiles: "./test-utils/vitest.setup.ts",
+    coverage: {
+      enabled: true,
+      provider: "v8",
+      reporter: ["text", "json", "json-summary", "html"],
+      reportOnFailure: true,
+      include: [
+        "components/**/*.ts",
+        "components/**/*.tsx",
+        "entrypoints/**/*.ts",
+        "entrypoints/**/*.tsx",
+        "utils/**/*.ts",
+        "utils/**/*.tsx",
+      ],
+      exclude: ["**/__tests__/*.*", "src/**/*.d.ts", "components/AppWrap.tsx", "utils/dayjs.ts"],
+      // TODO FIXME: enablewhen other tests are migrated
+      // thresholds: {
+      //   lines: 70,
+      //   statements: 70,
+      //   functions: 70,
+      //   branches: 80,
+      // },
+    },
   },
   plugins: [WxtVitest()],
 });
