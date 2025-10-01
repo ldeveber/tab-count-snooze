984ebb0c20d907c6b71c6414a3c8722664abf8e8
Lesley <lesley.deveber@gmail.com>
2025-10-01 13:09:18 -0400

    favicon improvements

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,14 @@
 import { defineAppConfig } from "#imports";
 
+// @see https://wxt.dev/guide/essentials/config/environment-variables.html#built-in-environment-variables
 declare module "wxt/utils/define-app-config" {
   export interface WxtAppConfig {
     isDev: boolean;
+    isChrome: boolean;
   }
 }
 
 export default defineAppConfig({
   isDev: import.meta.env.DEV,
+  isChrome: import.meta.env.CHROME,
 });
--- components/tabList/TabFavicon.tsx
+++ components/tabList/TabFavicon.tsx
@@ -1,19 +1,18 @@
 import { FileQuestionMarkIcon, PuzzleIcon } from "lucide-react";
+import { useAppConfig } from "wxt/utils/app-config";
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
-
-export const FAVICON_SIZE = 24;
 
 export function Loading() {
   return <Skeleton className="size-6 rounded-sm" />;
 }
 
-function FallbackIcon({ url }: { url: Browser.tabs.Tab["url"] }) {
+function FallbackIcon({ url }: { url?: Browser.tabs.Tab["url"] }) {
   if (url) {
-    if (url.startsWith("chrome-extension://")) {
-      return <PuzzleIcon className="size-6" />;
-    }
-    if (url.startsWith("chrome://extensions")) {
+    if (
+      url.startsWith("chrome-extension://") ||
+      url.startsWith("chrome://extensions")
+    ) {
       return <PuzzleIcon className="size-6" />;
     }
   }
@@ -25,21 +24,34 @@ export default function TabFavicon({
 }: {
   readonly tab: Browser.tabs.Tab;
 }) {
+  const { isChrome } = useAppConfig();
   const faded = tab.discarded || tab.status === "unloaded";
-  const favIconUrl =
-    tab.favIconUrl && tab.favIconUrl.length > 0 ? tab.favIconUrl : undefined;
+
+  const favicon = useMemo(() => {
+    if (isChrome && (tab.url || tab.pendingUrl)) {
+      // "/_favicon/" only available in chrome
+      // @ts-expect-error https://github.com/wxt-dev/wxt/issues/1559
+      const favicon = new URL(browser.runtime.getURL("/_favicon/"));
+      favicon.searchParams.set("pageUrl", tab.url ?? tab.pendingUrl ?? "");
+      favicon.searchParams.set("size", "32");
+      return favicon.toString();
+    } else if (tab.favIconUrl && tab.favIconUrl.length > 0) {
+      return tab.favIconUrl;
+    }
+  }, [isChrome, tab.url]);
+
   return (
     <span
       role="none"
       className={cn(
-        "flex size-6 items-center justify-center overflow-hidden bg-muted/60",
+        "flex size-6 items-center justify-center overflow-hidden",
         faded && "opacity-30",
       )}
     >
-      {favIconUrl ? (
+      {favicon ? (
         <img
-          src={favIconUrl}
-          alt=""
+          src={favicon}
+          alt="Favicon for tab"
           className="size-full object-cover"
           referrerPolicy="no-referrer"
           loading="lazy"
