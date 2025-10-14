4dbc03039b85cd90ba4bfa40c8bd40148d733361
Lesley <lesley.deveber@gmail.com>
2025-10-14 15:54:15 -0400

    fix app wrap and options container

--- components/options/Options.tsx
+++ components/options/Options.tsx
@@ -4,20 +4,20 @@ import { OptFooter } from "./OptFooter";
 export default function Options() {
   const { version } = useAppConfig();
   return (
-    <div className="flex h-auto min-h-screen flex-col gap-4 overflow-hidden">
-      <div className="flex flex-col gap-1.5 p-4">
+    <div className="flex flex-col gap-4 overflow-hidden">
+      <div className="fixed top-0 z-50 flex w-full flex-shrink flex-col gap-1.5 bg-background p-4">
         <h2 className="font-semibold text-2xl text-foreground">Settings</h2>
         <p className="text-muted-foreground text-sm">
           Tab Count Snooze version {version}
         </p>
       </div>
-      <div className="flex-1 overflow-y-auto px-4">
+      <div className="mt-22 mb-16 flex-grow overflow-y-auto p-4">
         <h1 className="font-semibold text-lg">Options</h1>
         <p className="text-muted-foreground text-sm">
           Options UI is under construction.
         </p>
       </div>
-      <div className="mt-auto flex flex-col gap-2 p-4">
+      <div className="fixed bottom-0 z-50 flex w-full flex-shrink flex-col gap-2 bg-background p-4">
         <OptFooter />
       </div>
     </div>
