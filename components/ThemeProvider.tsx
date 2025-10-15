bc2647ea0c42c570215c2034aa006e13d7bfc7fd
Lesley <lesley.deveber@gmail.com>
2025-10-15 13:09:58 -0400

    add tailwind indicator to help with breakpoint debugging

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { TailwindIndicator } from "./tailwind-indicator";
 
 export type Theme = "dark" | "light" | "system";
 
@@ -59,6 +60,7 @@ export function ThemeProvider({
   return (
     <ThemeProviderContext.Provider {...props} value={value}>
       {children}
+      <TailwindIndicator />
     </ThemeProviderContext.Provider>
   );
 }
